ae62cf05e62a251389c151aa7329d924550a38a7
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2022-10-14 19:45:28 +0100

    Added a script to get backups

--- project2/getBackups.sh
+++ project2/getBackups.sh
@@ -1,5 +1,5 @@
+rm backups/backup*.zip
 icat -o 2048 ../backup_disk.img 166013 > backups/backup_1665188803.zip
 icat -o 2048 ../backup_disk.img 166050 > backups/backup_1665189001.zip
 icat -o 2048 ../backup_disk.img 137361 > backups/backup_1665189601.zip
 icat -o 2048 ../backup_disk.img 137626 > backups/backup_1665190201.zip
-
